d3383ed61e219ea227f48f0479b64c1e453fdb95
ericcnguyen04 <ericc.win@gmail.com>
2024-10-26 03:22:24 -0400

    api

--- back-end/app.py
+++ back-end/app.py
@@ -4,6 +4,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app, origins='*')
 
+# Replace with your actual Gemini API URL and API key
+GEMINI_API_URL = "https://aistudio.google.com/prompts/new_chat"
+GEMINI_API_KEY = "AIzaSyA_NLgNcwTxCaudQLqnImLQqwb7FjVIBRQ"
+
+
 @app.route('/')
 def home():
     return "Hello, Flask is up and running!"
